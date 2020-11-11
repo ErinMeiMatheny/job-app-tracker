a5a3e22fe22fa59f3a67c59017a4353e7ff0e472
Erin Matheny <erin_m713@yahoo.com>
2020-11-10 19:02:05 -0600

    initialized sequelize cli and first model

---
+++ backend/models/job_track.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class job_track extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  job_track.init({
+    user_id: DataTypes.INTEGER,
+    company_name: DataTypes.STRING,
+    job_title: DataTypes.STRING,
+    date_applied: DataTypes.DATE,
+    city: DataTypes.STRING,
+    company_responded: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'job_track',
+  });
+  return job_track;
+};
